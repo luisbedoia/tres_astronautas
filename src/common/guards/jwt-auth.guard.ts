e53bf50e17156a04b0bda39662e8926503e5e1a7
Luis Arias <ariasb.luis@gmail.com>
2025-05-21 15:48:01 -0500

    type errors fixed

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -4,6 +4,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -11,7 +12,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err: any, user: any) {
+  handleRequest<TUser = JwtPayload>(
+    err: any,
+    user: TUser,
+    _info: any,
+    _context: ExecutionContext,
+    _status?: any,
+  ): TUser {
     if (err || !user) {
       throw err || new UnauthorizedException();
     }
